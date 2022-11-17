39ed597b289db83db0130bf988bfd2e989afd5cf
Tri Dao <tridpq@gmail.com>
2022-11-17 11:45:11 -0800

    [LayerNorm] Compile for both sm70 and sm80

--- csrc/layer_norm/setup.py
+++ csrc/layer_norm/setup.py
@@ -98,8 +98,8 @@ if os.path.exists(os.path.join(torch_dir, "include", "ATen", "CUDAGeneratorImpl.
 raise_if_cuda_home_none("--fast_layer_norm")
 # Check, if CUDA11 is installed for compute capability 8.0
 cc_flag = []
-# cc_flag.append("-gencode")
-# cc_flag.append("arch=compute_70,code=sm_70")
+cc_flag.append("-gencode")
+cc_flag.append("arch=compute_70,code=sm_70")
 cc_flag.append("-gencode")
 cc_flag.append("arch=compute_80,code=sm_80")
 
