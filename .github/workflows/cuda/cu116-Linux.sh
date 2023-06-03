a372e2be1bd970956bd9b2b8e84f23b7e86e2a4a
Pierce Freeman <piercefreeman@gmail.com>
2023-06-02 19:19:49 -0700

    Add CUDA 11.7

--- .github/workflows/cuda/cu116-Linux.sh
+++ .github/workflows/cuda/cu116-Linux.sh
@@ -7,6 +7,7 @@ OS=ubuntu${OS_VERSION}
 wget -nv https://developer.download.nvidia.com/compute/cuda/repos/${OS}/x86_64/cuda-${OS}.pin
 sudo mv cuda-${OS}.pin /etc/apt/preferences.d/cuda-repository-pin-600
 wget -nv https://developer.download.nvidia.com/compute/cuda/11.6.2/local_installers/cuda-repo-${OS}-11-6-local_11.6.2-510.47.03-1_amd64.deb
+
 sudo dpkg -i cuda-repo-${OS}-11-6-local_11.6.2-510.47.03-1_amd64.deb
 sudo apt-key add /var/cuda-repo-${OS}-11-6-local/7fa2af80.pub
 
