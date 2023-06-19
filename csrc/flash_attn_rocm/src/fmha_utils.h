777e1666c4f1d29098ee0c345db7ad13e75971bf
fsx950223 <fsx950223@outlook.com>
2023-06-19 21:47:18 +0800

    fix pt2.0 build

--- csrc/flash_attn_rocm/src/fmha_utils.h
+++ csrc/flash_attn_rocm/src/fmha_utils.h
@@ -96,9 +96,9 @@ static inline size_t get_size_in_bytes( size_t n, DataType dtype ) {
 
 static std::tuple<uint64_t, uint64_t> unpack(at::PhiloxCudaState arg) {
   if (arg.captured_) {
-    return std::make_tuple(arg.seed_, static_cast<uint64_t>(*(arg.offset_.ptr) + arg.offset_intragraph_));
+    return std::make_tuple(static_cast<uint64_t>(*arg.seed_.ptr), static_cast<uint64_t>(*(arg.offset_.ptr) + arg.offset_intragraph_));
   } else {
-    return std::make_tuple(arg.seed_, arg.offset_.val);
+    return std::make_tuple(arg.seed_.val, arg.offset_.val);
   }
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////////
