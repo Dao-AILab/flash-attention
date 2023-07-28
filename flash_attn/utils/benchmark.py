60499abcfda1fabd48e1b97341f4f888c66991a3
Tri Dao <tridpq@gmail.com>
2023-07-28 00:26:52 -1000

    [Benchmark] Add script to benchmark FlashAttention

--- flash_attn/utils/benchmark.py
+++ flash_attn/utils/benchmark.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2022, Tri Dao.
+# Copyright (c) 2023, Tri Dao.
 """ Useful functions for writing test code. """
 
 import torch
@@ -10,14 +10,12 @@ def benchmark_forward(fn, *inputs, repeats=10, desc='', verbose=True, amp=False,
     """ Use Pytorch Benchmark on the forward pass of an arbitrary function. """
     if verbose:
         print(desc, '- Forward pass')
-    def fn_amp(*inputs, **kwinputs):
+    def amp_wrapper(*inputs, **kwinputs):
         with torch.autocast(device_type='cuda', dtype=amp_dtype, enabled=amp):
             fn(*inputs, **kwinputs)
-    for _ in range(repeats):  # warmup
-        fn_amp(*inputs, **kwinputs)
     t = benchmark.Timer(
             stmt='fn_amp(*inputs, **kwinputs)',
-            globals={'fn_amp': fn_amp, 'inputs': inputs, 'kwinputs': kwinputs},
+            globals={'fn_amp': amp_wrapper, 'inputs': inputs, 'kwinputs': kwinputs},
             num_threads=torch.get_num_threads(),
             )
     m = t.timeit(repeats)
@@ -40,13 +38,18 @@ def benchmark_backward(fn, *inputs, grad=None, repeats=10, desc='', verbose=True
     else:
         if grad.shape != y.shape:
             raise RuntimeError('Grad shape does not match output shape')
-    for _ in range(repeats):  # warmup
+    def f(*inputs, y, grad):
+        # Set .grad to None to avoid extra operation of gradient accumulation
+        for x in inputs:
+            if isinstance(x, torch.Tensor):
+                x.grad = None
         y.backward(grad, retain_graph=True)
+
     t = benchmark.Timer(
-            stmt='y.backward(grad, retain_graph=True)',
-            globals={'y': y, 'grad': grad},
+            stmt='f(*inputs, y=y, grad=grad)',
+            globals={'f': f, 'inputs': inputs, 'y': y, 'grad': grad},
             num_threads=torch.get_num_threads(),
-        )
+            )
     m = t.timeit(repeats)
     if verbose:
         print(m)
@@ -58,19 +61,24 @@ def benchmark_combined(fn, *inputs, grad=None, repeats=10, desc='', verbose=True
     """ Use Pytorch Benchmark on the forward+backward pass of an arbitrary function. """
     if verbose:
         print(desc, '- Forward + Backward pass')
+    with torch.autocast(device_type='cuda', dtype=amp_dtype, enabled=amp):
+        y = fn(*inputs, **kwinputs)
+        if type(y) is tuple:
+            y = y[0]
+    if grad is None:
+        grad = torch.randn_like(y)
+    else:
+        if grad.shape != y.shape:
+            raise RuntimeError('Grad shape does not match output shape')
     def f(grad, *inputs, **kwinputs):
+        for x in inputs:
+            if isinstance(x, torch.Tensor):
+                x.grad = None
         with torch.autocast(device_type='cuda', dtype=amp_dtype, enabled=amp):
             y = fn(*inputs, **kwinputs)
             if type(y) is tuple:
                 y = y[0]
-        if grad is None:
-            grad = torch.randn_like(y)
-        else:
-            if grad.shape != y.shape:
-                raise RuntimeError('Grad shape does not match output shape')
         y.backward(grad, retain_graph=True)
-    for _ in range(repeats):  # warmup
-        f(grad, *inputs, **kwinputs)
     t = benchmark.Timer(
             stmt='f(grad, *inputs, **kwinputs)',
             globals={'f': f, 'fn': fn, 'inputs': inputs, 'grad': grad, 'kwinputs': kwinputs},
@@ -80,6 +88,17 @@ def benchmark_combined(fn, *inputs, grad=None, repeats=10, desc='', verbose=True
     if verbose:
         print(m)
     return t, m
+
+
+def benchmark_fwd_bwd(fn, *inputs, grad=None, repeats=10, desc='', verbose=True, amp=False,
+                      amp_dtype=torch.float16, **kwinputs):
+    """ Use Pytorch Benchmark on the forward+backward pass of an arbitrary function. """
+    return (
+        benchmark_forward(fn, *inputs, repeats=repeats, desc=desc, verbose=verbose,
+                          amp=amp, amp_dtype=amp_dtype, **kwinputs),
+        benchmark_backward(fn, *inputs, grad=grad, repeats=repeats, desc=desc, verbose=verbose,
+                           amp=amp, amp_dtype=amp_dtype, **kwinputs),
+    )
 
 
 def benchmark_all(fn, *inputs, grad=None, repeats=10, desc='', verbose=True, amp=False,
@@ -102,16 +121,15 @@ def pytorch_profiler(fn, *inputs, trace_filename=None, backward=False, amp=False
         with torch.autocast(device_type='cuda', dtype=amp_dtype, enabled=amp):
             g = torch.randn_like(fn(*inputs, **kwinputs))
     for _ in range(30):   # Warm up
+        if backward:
+            for x in inputs:
+                if isinstance(x, torch.Tensor):
+                    x.grad = None
         with torch.autocast(device_type='cuda', dtype=amp_dtype, enabled=amp):
-            if backward:
-                for x in inputs:
-                    if isinstance(x, torch.Tensor):
-                        x.grad = None
-            # fn(*inputs, **kwinputs) if not backward else fn(*inputs, **kwinputs).backward(g)
             out = fn(*inputs, **kwinputs)
         # Backward should be done outside autocast
         if backward:
-            out.backward(g)
+            out.backward(g, retain_graph=True)
     activities = ([torch.profiler.ProfilerActivity.CPU] if cpu else []) + [torch.profiler.ProfilerActivity.CUDA]
     with torch.profiler.profile(
         activities=activities,
@@ -119,13 +137,13 @@ def pytorch_profiler(fn, *inputs, trace_filename=None, backward=False, amp=False
         # profile_memory=True,
         with_stack=True,
     ) as prof:
+        if backward:
+            for x in inputs:
+                if isinstance(x, torch.Tensor):
+                    x.grad = None
         with torch.autocast(device_type='cuda', dtype=amp_dtype, enabled=amp):
-            if backward:
-                for x in inputs:
-                    if isinstance(x, torch.Tensor):
-                        x.grad = None
             out = fn(*inputs, **kwinputs)
-        if backward: out.backward(g)
+        if backward: out.backward(g, retain_graph=True)
     if verbose:
         # print(prof.key_averages().table(sort_by="self_cuda_time_total", row_limit=50))
         print(prof.key_averages().table(row_limit=50))
