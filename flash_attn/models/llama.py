078531130c5e023c1d33738f8e429fe80700318d
Xuechen Li <12689993+lxuechen@users.noreply.github.com>
2023-08-14 15:47:22 -0700

    revert style changes.

--- flash_attn/models/llama.py
+++ flash_attn/models/llama.py
@@ -16,13 +16,10 @@ from transformers import GPT2Config, LlamaConfig
 def remap_state_dict_meta_llama(state_dict, config):
     def key_mapping_layers(key):
         return f'transformer.{key}' if not key.startswith('output.') else key
-
     state_dict = OrderedDict((key_mapping_layers(k), v) for k, v in state_dict.items())
-
     # Word embedding
     def key_mapping_emb(key):
         return re.sub(r'^transformer.tok_embeddings.', 'transformer.embeddings.word_embeddings.', key)
-
     state_dict = OrderedDict((key_mapping_emb(k), v) for k, v in state_dict.items())
     word_embeddings = state_dict.pop('transformer.embeddings.word_embeddings.weight')
     # It's possible that vocab_size is padded to be a multiple of 8, for example.
@@ -39,7 +36,7 @@ def remap_state_dict_meta_llama(state_dict, config):
         # Need to recompute vocab_size since LLaMa shards the word embeddings and output embeddings
         # differently.
         vocab_size = (math.ceil(output_embeddings.shape[0] / pad_vocab_size_multiple)
-                      * pad_vocab_size_multiple)
+                    * pad_vocab_size_multiple)
         # It's possible that vocab_size is padded to be a multiple of 8, for example.
         state_dict['lm_head.weight'] = F.pad(
             output_embeddings, (0, 0, 0, vocab_size - output_embeddings.shape[0])
@@ -51,7 +48,6 @@ def remap_state_dict_meta_llama(state_dict, config):
         key = re.sub(r'^transformer.layers.(\d+).attention_norm.', r'transformer.layers.\1.norm1.', key)
         key = re.sub(r'^transformer.layers.(\d+).ffn_norm.', r'transformer.layers.\1.norm2.', key)
         return key
-
     state_dict = OrderedDict((key_mapping_ln(k), v) for k, v in state_dict.items())
 
     # MLP
@@ -60,11 +56,9 @@ def remap_state_dict_meta_llama(state_dict, config):
         w3 = state_dict.pop(f'transformer.layers.{l}.feed_forward.w3.weight')
         # Our ordering is different
         state_dict[f'transformer.layers.{l}.mlp.fc1.weight'] = torch.cat([w3, w1], dim=0)
-
     def key_mapping_mlp(key):
         return re.sub(r'^transformer.layers.(\d+).feed_forward.w2.',
                       r'transformer.layers.\1.mlp.fc2.', key)
-
     state_dict = OrderedDict((key_mapping_mlp(k), v) for k, v in state_dict.items())
 
     # Attention
@@ -75,11 +69,9 @@ def remap_state_dict_meta_llama(state_dict, config):
         state_dict[f'transformer.layers.{l}.mixer.Wqkv.weight'] = torch.cat([Wq, Wk, Wv], dim=0)
         # We don't store these
         state_dict.pop(f'transformer.layers.{l}.attention.inner_attention.rope.freqs', None)
-
     def key_mapping_attn(key):
         return re.sub(r'^transformer.layers.(\d+).attention.wo.',
                       r'transformer.layers.\1.mixer.out_proj.', key)
-
     state_dict = OrderedDict((key_mapping_attn(k), v) for k, v in state_dict.items())
 
     return state_dict
