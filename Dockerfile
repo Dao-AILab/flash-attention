d5e9dfe5805a3975c4ac1ca31f6b7617a3eb2325
Lixun Zhang <lixun.zhang@amd.com>
2023-08-25 21:51:07 -0500

    Benchmark FA fwd with OpenAI Triton on AMD GPUs

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+FROM rocm/pytorch:latest-release
+
+WORKDIR /workspace
+USER root
+
+# Download and install triton
+RUN git clone -b fa_fwd_benchmark_2gpus https://github.com/ROCmSoftwarePlatform/triton.git
+RUN cd /workspace/triton/python \
+    && pip install -e .
+
+# git clone flash-attention and checkout branch benchmark_openai_triton_amd
+RUN git clone -b benchmark_openai_triton_amd https://github.com/Dao-AILab/flash-attention.git
+
+# install gitpython
+RUN pip install gitpython
+# install bc
+RUN apt install bc
+
+ENV PYTHONPATH "/workspace/flash-attention"
---
+++ build_and_run.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+echo "Build docker image"
+docker build -t triton_fa_rocm .
+
+echo "Start container"
+docker run -it -d --device=/dev/kfd --device=/dev/dri --group-add video --name triton_fa_benchmark triton_fa_rocm
+
+echo "Benchmarking flash attention forward kernel with 1 GCD"
+docker exec --workdir /workspace/flash-attention/ triton_fa_benchmark python3 benchmarks/benchmark_flash_attention_forward.py
+
+echo "Benchmarking flash attention forward kernel with 2 GCDs"
+docker exec --workdir /workspace/triton/ triton_fa_benchmark ./scripts/amd/run_2gcd.sh
