87c9cfb49450c5db654f673f84417875eebbd3a8
Federico Berto <berto.federico2@gmail.com>
2023-09-19 03:35:30 +0900

    Add nvcc note on bare_metal_version `RuntimeError`

--- setup.py
+++ setup.py
@@ -107,7 +107,8 @@ if not SKIP_CUDA_BUILD:
     if CUDA_HOME is not None:
         _, bare_metal_version = get_cuda_bare_metal_version(CUDA_HOME)
         if bare_metal_version < Version("11.6"):
-            raise RuntimeError("FlashAttention is only supported on CUDA 11.6 and above")
+            raise RuntimeError("FlashAttention is only supported on CUDA 11.6 and above.  "
+                               "Note: make sure nvcc has a supported version by running nvcc -V.")    
     # cc_flag.append("-gencode")
     # cc_flag.append("arch=compute_75,code=sm_75")
     cc_flag.append("-gencode")
