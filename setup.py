bff3147175e2f05ae0a4d44495aef97ca45ce531
Tri Dao <tridpq@gmail.com>
2023-09-21 23:55:25 -0700

    Re-enable compilation for Hopper

--- setup.py
+++ setup.py
@@ -122,10 +122,10 @@ if not SKIP_CUDA_BUILD:
     # cc_flag.append("arch=compute_75,code=sm_75")
     cc_flag.append("-gencode")
     cc_flag.append("arch=compute_80,code=sm_80")
-    # if CUDA_HOME is not None:
-    #     if bare_metal_version >= Version("11.8"):
-    #         cc_flag.append("-gencode")
-    #         cc_flag.append("arch=compute_90,code=sm_90")
+    if CUDA_HOME is not None:
+        if bare_metal_version >= Version("11.8"):
+            cc_flag.append("-gencode")
+            cc_flag.append("arch=compute_90,code=sm_90")
 
     # HACK: The compiler flag -D_GLIBCXX_USE_CXX11_ABI is set to be the same as
     # torch._C._GLIBCXX_USE_CXX11_ABI
