5e525a8dc8a473f418c8aaf82f5322eb225d9ee5
Tri Dao <tridpq@gmail.com>
2023-10-03 22:18:11 -0700

    [CI] Use official Pytorch 2.1, add CUDA 11.8 for Pytorch 2.1

--- setup.py
+++ setup.py
@@ -233,7 +233,8 @@ def get_wheel_url():
     # _, cuda_version_raw = get_cuda_bare_metal_version(CUDA_HOME)
     torch_cuda_version = parse(torch.version.cuda)
     torch_version_raw = parse(torch.__version__)
-    if torch_version_raw.major == 2 and torch_version_raw.minor == 1:
+    # Workaround for nvcc 12.1 segfaults when compiling with Pytorch 2.1
+    if torch_version_raw.major == 2 and torch_version_raw.minor == 1 and torch_cuda_version.major == 12:
         torch_cuda_version = parse("12.2")
     python_version = f"cp{sys.version_info.major}{sys.version_info.minor}"
     platform_name = get_platform()
