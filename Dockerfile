e1ecbe976ca4d881c185de44d771ababb6bd8d1b
Lixun Zhang <lixun.zhang@amd.com>
2023-10-11 20:35:18 -0500

    Update benchmark command
    - Only the triton repo is needed to benchmark with 1 and 2 GCDs
    - Added D=128 support

--- Dockerfile
+++ Dockerfile
@@ -8,11 +8,6 @@ RUN git clone -b fa_fwd_benchmark_2gpus https://github.com/ROCmSoftwarePlatform/
 RUN cd /workspace/triton/python \
     && pip install -e .
 
-# git clone flash-attention and checkout branch benchmark_openai_triton_amd
-RUN git clone -b benchmark_openai_triton_amd https://github.com/zhanglx13/flash-attention.git
-
 RUN pip install gitpython
 RUN apt install bc
 RUN pip install einops
-
-ENV PYTHONPATH "/workspace/flash-attention"
--- build_and_run.sh
+++ build_and_run.sh
@@ -7,7 +7,7 @@ echo "Start container"
 docker run -it -d --device=/dev/kfd --device=/dev/dri --group-add video --name triton_fa_benchmark triton_fa_rocm
 
 echo "Benchmarking flash attention forward kernel with 1 GCD"
-docker exec --workdir /workspace/flash-attention/ triton_fa_benchmark python3 benchmarks/benchmark_flash_attention_forward.py
+docker exec --workdir /workspace/triton/python/ triton_fa_benchmark python3 ./tutorials/06-fused-attention.py
 
 echo "Benchmarking flash attention forward kernel with 2 GCDs"
 docker exec --workdir /workspace/triton/ triton_fa_benchmark ./scripts/amd/run_2gcd.sh fwd
