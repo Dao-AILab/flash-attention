89e44a0351aaf4f9da9e6b3d6d532d45aaa26767
Junhao Zhang <regnnighe@gmail.com>
2023-10-18 23:31:14 +0800

    initilize vectors

--- csrc/flash_attn_rocm/src/params.hpp
+++ csrc/flash_attn_rocm/src/params.hpp
@@ -178,19 +178,19 @@ struct BatchedParams : public BaseParams {
 
     // Q layout [b, seqlen_q, h, d]
     std::vector<Index> q_lengths{b, h, seqlen_q, d};
-    std::vector<Index> q_strides = std::vector<Index>{q_batch_stride, q_head_stride, q_seq_stride, 1};
+    std::vector<Index> q_strides{q_batch_stride, q_head_stride, q_seq_stride, 1};
 
     // K layout [b, seqlen_kv, h, d]
     std::vector<Index> k_lengths{b, h, seqlen_k, d};
-    std::vector<Index> k_strides = std::vector<Index>{kv_batch_stride, kv_head_stride, kv_seq_stride, 1};
+    std::vector<Index> k_strides{kv_batch_stride, kv_head_stride, kv_seq_stride, 1};
 
     // V layout [b, seqlen_kv, h, d]
     std::vector<Index> v_lengths{b, h, d, seqlen_k};
-    std::vector<Index> v_strides = std::vector<Index>{kv_batch_stride, kv_head_stride, 1, kv_seq_stride};
+    std::vector<Index> v_strides{kv_batch_stride, kv_head_stride, 1, kv_seq_stride};
 
     // Y layout [b, seqlen_q, h, O]
     std::vector<Index> out_lengths{b, h, seqlen_q, d};
-    std::vector<Index> out_strides = std::vector<Index>{out_batch_stride, out_head_stride, out_seq_stride, 1};
+    std::vector<Index> out_strides{out_batch_stride, out_head_stride, out_seq_stride, 1};
 
     std::vector<Index> z_lengths{b, h, seqlen_q, seqlen_k};
     std::vector<Index> z_strides = 
@@ -436,26 +436,26 @@ struct GroupedParams : public BaseParams {
 
       // Q layout [b, seqlen_q, h, d]
       std::vector<Index> q_lengths{1, h, curr_seqlen_q, d};
-      std::vector<Index> q_strides = std::vector<Index>{curr_q_batch_stride, q_head_stride, q_seq_stride, 1};
+      std::vector<Index> q_strides{curr_q_batch_stride, q_head_stride, q_seq_stride, 1};
  
       // K layout [b, seqlen_kv, h, d]
       std::vector<Index> k_lengths{1, h, curr_seqlen_kv, d};
-      std::vector<Index> k_strides = std::vector<Index>{curr_kv_batch_stride, kv_head_stride, kv_seq_stride, 1};
+      std::vector<Index> k_strides{curr_kv_batch_stride, kv_head_stride, kv_seq_stride, 1};
 
       // V layout [b, seqlen_kv, h, d]
       std::vector<Index> v_lengths{1, h, d, curr_seqlen_kv};
-      std::vector<Index> v_strides = std::vector<Index>{curr_kv_batch_stride, kv_head_stride, 1, kv_seq_stride};
+      std::vector<Index> v_strides{curr_kv_batch_stride, kv_head_stride, 1, kv_seq_stride};
 
       // Y layout [b, seqlen_q, h, O]
       std::vector<Index> out_lengths{1, h, curr_seqlen_q, d};
-      std::vector<Index> out_strides = std::vector<Index>{curr_out_batch_stride, out_head_stride, out_seq_stride, 1};
+      std::vector<Index> out_strides{curr_out_batch_stride, out_head_stride, out_seq_stride, 1};
 
       std::vector<Index> z_lengths{1, h, curr_seqlen_q, curr_seqlen_kv};
       std::vector<Index> z_strides = 
-          z_permute
-          ? std::vector<Index>{h*curr_seqlen_q*curr_seqlen_kv, curr_seqlen_kv, h*curr_seqlen_kv, 1}
+          z_permute ? 
+          std::vector<Index>{h*curr_seqlen_q*curr_seqlen_kv, curr_seqlen_kv, h*curr_seqlen_kv, 1} :
           // Z layout [b, seqlen_q, h, seqlen_kv]
-          : std::vector<Index>{h*curr_seqlen_q*curr_seqlen_kv, curr_seqlen_q*curr_seqlen_kv, curr_seqlen_kv, 1};
+          std::vector<Index>{h*curr_seqlen_q*curr_seqlen_kv, curr_seqlen_q*curr_seqlen_kv, curr_seqlen_kv, 1};
           // Z layout [b, h, seqlen_q, seqlen_kv]
 
       // LSE layout [b, h, seqlen_q]
