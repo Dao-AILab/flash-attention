cd463f940e94272f408f0d798088bb16d2bc3ea4
Junhao Zhang <regnnighe@gmail.com>
2023-10-24 16:44:15 +0000

    sync interface

--- tests/test_flash_attn.py
+++ tests/test_flash_attn.py
@@ -10,7 +10,7 @@ from einops import rearrange, repeat
 from flash_attn import flash_attn_func, flash_attn_kvpacked_func, flash_attn_qkvpacked_func
 from flash_attn import flash_attn_varlen_qkvpacked_func, flash_attn_varlen_kvpacked_func
 from flash_attn import flash_attn_varlen_func
-from flash_attn.flash_attn_interface import _get_block_size_rocm
+from flash_attn.flash_attn_interface import _get_block_size
 from flash_attn.bert_padding import unpad_input, pad_input, index_first_axis
 
 
