2423cca3ad20c98ad452c7944feca3c222b88668
Brian Hirsh <briandhirsh@gmail.com>
2024-02-10 04:01:27 -0500

    fix backward for when query and key have different contiguity (#818)

--- csrc/flash_attn/flash_api.cpp
+++ csrc/flash_attn/flash_api.cpp
@@ -830,7 +830,7 @@ mha_bwd(const at::Tensor &dout,  // batch_size x seqlen_q x num_heads, x head_si
         TORCH_CHECK(dv.stride(-1) == 1, "dv must have contiguous last dimension");
         CHECK_SHAPE(dv, batch_size, seqlen_k, num_heads_k, head_size);
     } else {
-        dv = torch::empty_like(k);
+        dv = torch::empty_like(v);
     }
 
     at::Tensor dout_padded;
