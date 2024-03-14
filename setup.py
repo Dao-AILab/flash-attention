50896ec57424f3ebe7977c0f10067887ec40b627
Chirag Jain <jain.chirag925@gmail.com>
2024-03-14 09:16:57 +0530

    Make nvcc threads configurable via environment variable (#885)

--- setup.py
+++ setup.py
@@ -83,7 +83,8 @@ def check_if_cuda_home_none(global_option: str) -> None:
 
 
 def append_nvcc_threads(nvcc_extra_args):
-    return nvcc_extra_args + ["--threads", "4"]
+    nvcc_threads = os.getenv("NVCC_THREADS") or "4"
+    return nvcc_extra_args + ["--threads", nvcc_threads]
 
 
 cmdclass = {}
