26c9e82743e5f65cab4e09898995cac70c3de267
Arvind Sundararajan <arvinds@gmail.com>
2024-03-13 21:57:20 -0700

    Support ARM builds (#757)

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ def get_platform():
     Returns the platform name as used in wheel filenames.
     """
     if sys.platform.startswith("linux"):
-        return "linux_x86_64"
+        return f'linux_{platform.uname().machine}'
     elif sys.platform == "darwin":
         mac_version = ".".join(platform.mac_ver()[0].split(".")[:2])
         return f"macosx_{mac_version}_x86_64"
