d20d75a80982c5a725cda26fda7af220645c450d
Corey James Levinson <thecoreylevinson@gmail.com>
2024-05-24 08:06:05 -0400

    add exception to Timeout Error
    When timeout connecting, you get URLError: <urlopen error timed out>, In that case, build it from source.

--- setup.py
+++ setup.py
@@ -282,7 +282,7 @@ class CachedWheelsCommand(_bdist_wheel):
             wheel_path = os.path.join(self.dist_dir, archive_basename + ".whl")
             print("Raw wheel path", wheel_path)
             os.rename(wheel_filename, wheel_path)
-        except urllib.error.HTTPError:
+        except (urllib.error.HTTPError, urllib.error.URLError):
             print("Precompiled wheel not found. Building from source...")
             # If the wheel could not be downloaded, build from source
             super().run()
