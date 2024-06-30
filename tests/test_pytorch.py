af64c940453ba17b1d64b57455286913661865a2
Jianping Li <lijp@shanghaitech.edu.cn>
2024-06-30 12:44:27 +0800

    compare Tri version and pytorch version for flash-attention

--- tests/test_pytorch.py
+++ tests/test_pytorch.py
@@ -1,5 +1,7 @@
 import torch
 import torch.nn.functional as F
+import torch.utils.benchmark as benchmark
+
 import time
 from flash_attn import (
     flash_attn_func,
@@ -11,24 +13,40 @@ from flash_attn import (
     flash_attn_with_kvcache,
 )
 
+# Function to measure time and memory
+def benchmark_torch_function_in_microseconds(f, *args, **kwargs):
+    t0 = benchmark.Timer(
+        stmt="f(*args, **kwargs)", globals={"args": args, "kwargs": kwargs, "f": f}
+    )
+    return t0.blocked_autorange().mean * 1e6
+
+def measure_time_and_memory(func, *args):
+    torch.cuda.reset_peak_memory_stats()
+    start_time = time.time()
+    output = func(*args)
+    torch.cuda.synchronize()
+    end_time = time.time()
+    max_memory = torch.cuda.max_memory_allocated()
+    return end_time - start_time, max_memory, output
+
 
 batch_size = 32
 seq_len = 128
 head_size = 64
 
-q = torch.randn(batch_size, seq_len, head_size).cuda()
-k = torch.randn(batch_size, seq_len, head_size).cuda()
-v = torch.randn(batch_size, seq_len, head_size).cuda()
-
 # Optionally use the context manager to ensure one of the fused kernels is run
-query = torch.rand(32, 8, 128, 64, dtype=torch.float16, device="cuda")
-key = torch.rand(32, 8, 128, 64, dtype=torch.float16, device="cuda")
-value = torch.rand(32, 8, 128, 64, dtype=torch.float16, device="cuda")
+q = torch.rand(32, 8, 128, 64, dtype=torch.float16, device="cuda")
+k = torch.rand(32, 8, 128, 64, dtype=torch.float16, device="cuda")
+v = torch.rand(32, 8, 128, 64, dtype=torch.float16, device="cuda")
+
+# test for pytorch version of flash-attention
 with torch.backends.cuda.sdp_kernel(enable_math=False):
-    output = F.scaled_dot_product_attention(query,key,value)
+    output = F.scaled_dot_product_attention(q,k,v)
 
 print(f"pytorch implementaion of flashAttention is output.shape")
 
+
+# test for tri version of flash-attention
 out, lse, S_dmask = flash_attn_func(
             q,
             k,
@@ -39,12 +57,3 @@ out, lse, S_dmask = flash_attn_func(
 print(f"Tri version of flashAttention: \n out.shape")
 
 
-# Function to measure time and memory
-def measure_time_and_memory(func, *args):
-    torch.cuda.reset_peak_memory_stats()
-    start_time = time.time()
-    output = func(*args)
-    torch.cuda.synchronize()
-    end_time = time.time()
-    max_memory = torch.cuda.max_memory_allocated()
-    return end_time - start_time, max_memory, output
