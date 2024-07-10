7bfa488c4717509f65084b14fd4cfd5271b277ca
lucidrains <lucidrains@gmail.com>
2024-07-10 14:50:28 +0000

    restore another file

--- csrc/flash_attn/src/static_switch.h
+++ csrc/flash_attn/src/static_switch.h
@@ -81,13 +81,37 @@
     if (COND) {                              \
       using elem_type = cutlass::half_t;     \
       return __VA_ARGS__();                  \
+    } else {                                 \
+      using elem_type = cutlass::bfloat16_t; \
+      return __VA_ARGS__();                  \
     }                                        \
   }()
 
 #define HEADDIM_SWITCH(HEADDIM, ...)   \
   [&] {                                    \
-    if (HEADDIM <= 64) {                   \
+    if (HEADDIM <= 32) {                   \
+      constexpr static int kHeadDim = 32;  \
+      return __VA_ARGS__();                \
+    } else if (HEADDIM <= 64) {            \
       constexpr static int kHeadDim = 64;  \
+      return __VA_ARGS__();                \
+    } else if (HEADDIM <= 96) {            \
+      constexpr static int kHeadDim = 96;  \
+      return __VA_ARGS__();                \
+    } else if (HEADDIM <= 128) {           \
+      constexpr static int kHeadDim = 128; \
+      return __VA_ARGS__();                \
+    } else if (HEADDIM <= 160) {           \
+      constexpr static int kHeadDim = 160; \
+      return __VA_ARGS__();                \
+    } else if (HEADDIM <= 192) {           \
+      constexpr static int kHeadDim = 192; \
+      return __VA_ARGS__();                \
+    } else if (HEADDIM <= 224) {           \
+      constexpr static int kHeadDim = 224; \
+      return __VA_ARGS__();                \
+    } else if (HEADDIM <= 256) {           \
+      constexpr static int kHeadDim = 256; \
       return __VA_ARGS__();                \
     }                                      \
   }()
