0aabb1c94004ce425e63adf6d4fb758062f9e051
Jianping Li <lijp@shanghaitech.edu.cn>
2024-08-13 15:08:16 -0400

    source built

--- setup.py
+++ setup.py
@@ -269,26 +269,26 @@ class CachedWheelsCommand(_bdist_wheel):
 
         wheel_url, wheel_filename = get_wheel_url()
         print("Guessing wheel URL: ", wheel_url)
-        try:
-            urllib.request.urlretrieve(wheel_url, wheel_filename)
+        # try:
+        #     urllib.request.urlretrieve(wheel_url, wheel_filename)
 
-            # Make the archive
-            # Lifted from the root wheel processing command
-            # https://github.com/pypa/wheel/blob/cf71108ff9f6ffc36978069acb28824b44ae028e/src/wheel/bdist_wheel.py#LL381C9-L381C85
-            if not os.path.exists(self.dist_dir):
-                os.makedirs(self.dist_dir)
+        #     # Make the archive
+        #     # Lifted from the root wheel processing command
+        #     # https://github.com/pypa/wheel/blob/cf71108ff9f6ffc36978069acb28824b44ae028e/src/wheel/bdist_wheel.py#LL381C9-L381C85
+        #     if not os.path.exists(self.dist_dir):
+        #         os.makedirs(self.dist_dir)
 
-            impl_tag, abi_tag, plat_tag = self.get_tag()
-            archive_basename = f"{self.wheel_dist_name}-{impl_tag}-{abi_tag}-{plat_tag}"
+        #     impl_tag, abi_tag, plat_tag = self.get_tag()
+        #     archive_basename = f"{self.wheel_dist_name}-{impl_tag}-{abi_tag}-{plat_tag}"
 
-            wheel_path = os.path.join(self.dist_dir, archive_basename + ".whl")
-            print("Raw wheel path", wheel_path)
-            # os.rename(wheel_filename, wheel_path)
-            shutil.move(wheel_filename, wheel_path)
-        except (urllib.error.HTTPError, urllib.error.URLError):
-            print("Precompiled wheel not found. Building from source...")
-            # If the wheel could not be downloaded, build from source
-            super().run()
+        #     wheel_path = os.path.join(self.dist_dir, archive_basename + ".whl")
+        #     print("Raw wheel path", wheel_path)
+        #     # os.rename(wheel_filename, wheel_path)
+        #     shutil.move(wheel_filename, wheel_path)
+        # except (urllib.error.HTTPError, urllib.error.URLError):
+        print("Building from source...")
+        # If the wheel could not be downloaded, build from source
+        super().run()
 
 
 class NinjaBuildExtension(BuildExtension):
