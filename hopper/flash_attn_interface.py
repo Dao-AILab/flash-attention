099ca2848873f09fba52e3ee8fd8a3c28e25db07
Ganesh Bikshandi <ganesh@colfax-intl.com>
2024-09-20 18:30:28 -0700

    add gqa decoding logic.

--- hopper/flash_attn_interface.py
+++ hopper/flash_attn_interface.py
@@ -469,7 +469,7 @@ def flash_attn_with_kvcache(
     alibi_slopes=None,
     num_splits=0,
     return_softmax_lse=False,
-    gqa_decoding=False,
+    gqa_decoding=None,
 ):
     """
     If k and v are not None, k_cache and v_cache will be updated *inplace* with the new values from
@@ -570,6 +570,10 @@ def flash_attn_with_kvcache(
         cache_seqlens = maybe_contiguous(cache_seqlens)
     cache_batch_idx = maybe_contiguous(cache_batch_idx)
     block_table = maybe_contiguous(block_table)
+    if gqa_decoding is None:
+        use_gqa_decoding = True if q.shape[1] <=64 else False
+    else:
+        use_gqa_decoding = gqa_decoding
     out, softmax_lse = flashattn_hopper_cuda.fwd_kvcache(
         q,
         k_cache,
@@ -591,6 +595,6 @@ def flash_attn_with_kvcache(
         softcap,
         rotary_interleaved,
         num_splits,
-        gqa_decoding
+        use_gqa_decoding
     )
     return (out, softmax_lse) if return_softmax_lse else out
