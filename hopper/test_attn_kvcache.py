ffa48eb8cc05eb0872d3e0ad734e3c455eefafc6
Ganesh Bikshandi <ganesh@colfax-intl.com>
2024-09-24 17:19:38 -0700

    more refactoring.

--- hopper/test_attn_kvcache.py
+++ hopper/test_attn_kvcache.py
@@ -1,20 +1,127 @@
 import pytest
+from einops import rearrange, repeat
 import torch
 import flash_attn
 import flash_attn_interface
 import itertools
+import math
 import time
 
-@pytest.mark.parametrize("nheads_q", [48//2, 64//8, 16, 48, 64//4])
-@pytest.mark.parametrize("nheads_kv", [4//2, 8//8, 1, 4, 8//4])
+def attention_ref(
+    q,
+    k,
+    v,
+    query_padding_mask=None,
+    key_padding_mask=None,
+    attn_bias=None,
+    dropout_p=0.0,
+    dropout_mask=None,
+    causal=False,
+    window_size=(-1, -1),  # -1 means infinite window size
+    softcap=0.0,
+    upcast=True,
+    reorder_ops=False,
+    key_leftpad=None,
+):
+    """
+    Arguments:
+        q: (batch_size, seqlen_q, nheads, head_dim)
+        k: (batch_size, seqlen_k, nheads_k, head_dim)
+        v: (batch_size, seqlen_k, nheads_k, head_dim)
+        query_padding_mask: (batch_size, seqlen_q)
+        key_padding_mask: (batch_size, seqlen_k)
+        attn_bias: broadcastable to (batch_size, nheads, seqlen_q, seqlen_k)
+        dropout_p: float
+        dropout_mask: (batch_size, nheads, seqlen_q, seqlen_k)
+        causal: whether to apply causal masking
+        window_size: (int, int), left and right window size
+        upcast: whether to cast all inputs to fp32, do all computation in fp32, then cast
+            output back to fp16/bf16.
+        reorder_ops: whether to change the order of operations (scaling k instead of scaling q, etc.)
+            without changing the math. This is to estimate the numerical error from operation
+            reordering.
+    Output:
+        output: (batch_size, seqlen_q, nheads, head_dim)
+        attention: (batch_size, nheads, seqlen_q, seqlen_k), softmax after dropout
+    """
+    if causal:
+        window_size = (window_size[0], 0)
+    dtype_og = q.dtype
+    if upcast:
+        q, k, v = q.float(), k.float(), v.float()
+    seqlen_q, seqlen_k = q.shape[1], k.shape[1]
+    k = repeat(k, "b s h d -> b s (h g) d", g=q.shape[2] // k.shape[2])
+    v = repeat(v, "b s h d -> b s (h g) d", g=q.shape[2] // v.shape[2])
+    d = q.shape[-1]
+    if not reorder_ops:
+        scores = torch.einsum("bthd,bshd->bhts", q / math.sqrt(d), k)
+    else:
+        scores = torch.einsum("bthd,bshd->bhts", q, k / math.sqrt(d))
+    if softcap > 0:
+        scores = scores / softcap
+        scores = scores.tanh()
+        scores = scores * softcap
+    if key_padding_mask is not None:
+        scores.masked_fill_(rearrange(~key_padding_mask, "b s -> b 1 1 s"), float("-inf"))
+    if window_size[0] >= 0 or window_size[1] >= 0:
+        local_mask = construct_local_mask(
+            seqlen_q,
+            seqlen_k,
+            window_size,
+            query_padding_mask,
+            key_padding_mask,
+            q.device,
+            key_leftpad=key_leftpad,
+        )
+        scores.masked_fill_(local_mask, float("-inf"))
+    if attn_bias is not None:
+        scores = scores + attn_bias
+    attention = torch.softmax(scores, dim=-1).to(v.dtype)
+    # Some rows might be completely masked out so we fill them with zero instead of NaN
+    if window_size[0] >= 0 or window_size[1] >= 0:
+        attention = attention.masked_fill(torch.all(local_mask, dim=-1, keepdim=True), 0.0)
+    # We want to mask here so that the attention matrix doesn't have any NaNs
+    # Otherwise we'll get NaN in dV
+    if query_padding_mask is not None:
+        attention = attention.masked_fill(rearrange(~query_padding_mask, "b s -> b 1 s 1"), 0.0)
+    dropout_scaling = 1.0 / (1 - dropout_p)
+    # attention_drop = attention.masked_fill(~dropout_mask, 0.0) * dropout_scaling
+    # output = torch.einsum('bhts,bshd->bthd', attention_drop , v)
+    if dropout_mask is not None:
+        attention_drop = attention.masked_fill(~dropout_mask, 0.0)
+    else:
+        attention_drop = attention
+    output = torch.einsum("bhts,bshd->bthd", attention_drop, v * dropout_scaling)
+    if query_padding_mask is not None:
+        output.masked_fill_(rearrange(~query_padding_mask, "b s -> b s 1 1"), 0.0)
+    return output.to(dtype=dtype_og), attention.to(dtype=dtype_og)
+
+
 @pytest.mark.parametrize("num_requests", [1, 4, 16])
 @pytest.mark.parametrize("query_seqlen", [1, 16])
 @pytest.mark.parametrize("context_seqlen", [16384, 65536])
 @pytest.mark.parametrize("headdim", [64, 128, 256])
-def test_flash_attn_kvcach_output(nheads_q, nheads_kv, num_requests, query_seqlen, context_seqlen, headdim):
+@pytest.mark.parametrize(
+    "nheads_kv, gqa_ratio",
+    [
+        (24, 2),
+        (8, 1),
+        (16, 1),
+        (48, 4),
+        (16, 2),
+    ],
+)
+#@pytest.mark.parametrize("nheads_q", [24])
+#@pytest.mark.parametrize("nheads_kv", [4//2])
+#@pytest.mark.parametrize("num_requests", [1])
+#@pytest.mark.parametrize("query_seqlen", [1])
+#@pytest.mark.parametrize("context_seqlen", [16384])
+#@pytest.mark.parametrize("headdim", [128])
+def test_flash_attn_kvcach_output(nheads_kv, gqa_ratio, num_requests, query_seqlen, context_seqlen, headdim):
     device = "cuda"
     num_caches = 16
     cache_seqlen = 65536
+    nheads_q = nheads_kv * gqa_ratio
 
     k_cache = torch.randn(
         (num_caches, cache_seqlen, nheads_kv, headdim), device="cuda", dtype=torch.bfloat16
@@ -27,7 +134,15 @@ def test_flash_attn_kvcach_output(nheads_q, nheads_kv, num_requests, query_seqle
     cache_idxs = torch.randperm(num_caches, dtype=torch.int32, device="cuda")[:num_requests]
     cache_seqlens = torch.tensor([context_seqlen] * num_requests, dtype=torch.int32, device="cuda")
     torch.cuda.synchronize()
-    out_fa2, lse_fa2 = flash_attn_interface.flash_attn_with_kvcache(
+
+    #out_ref, = attention_ref(
+    #    q,
+    #    k_cache,
+    #    v_cache,
+    #    causal=True,
+    #)
+
+    out_ref, lse_ref = flash_attn_interface.flash_attn_with_kvcache(
                     q=q,
                     k_cache=k_cache,
                     v_cache=v_cache,
@@ -37,6 +152,7 @@ def test_flash_attn_kvcach_output(nheads_q, nheads_kv, num_requests, query_seqle
                     num_splits=1,
                     return_softmax_lse=True,
                 )
+
     for i in range(1, 128):
                 out_fa3, lse_fa3 = flash_attn_interface.flash_attn_with_kvcache(
                     q=q,
@@ -62,23 +178,23 @@ def test_flash_attn_kvcach_output(nheads_q, nheads_kv, num_requests, query_seqle
                 )
 
                 torch.cuda.synchronize()
-                print ('output-max-diff', i, context_seqlen, (out_fa2 - out_fa3).abs().max().item())
-                print ('output-mean-diff',i, context_seqlen, (out_fa2 - out_fa3).abs().mean().item())
-                print ('output-max-diff gqa', i, context_seqlen, (out_fa2 - out_fa3_gqa).abs().max().item())
-                print ('output-mean-diff gqa',i, context_seqlen, (out_fa2 - out_fa3_gqa).abs().mean().item())
-                print ('lse-max-diff',i, context_seqlen, (lse_fa2 - lse_fa3).abs().max().item())
-                print ('lse-mean-diff',i,  context_seqlen, (lse_fa2 - lse_fa3).abs().mean().item())
-                print ('lse-max-diff gqa',i, context_seqlen, (lse_fa2 - lse_fa3_gqa).abs().max().item())
-                print ('lse-mean-diff gqa',i, context_seqlen, (lse_fa2 - lse_fa3_gqa).abs().mean().item())
+                print ('output-max-diff', i, context_seqlen, (out_ref - out_fa3).abs().max().item())
+                print ('output-mean-diff',i, context_seqlen, (out_ref - out_fa3).abs().mean().item())
+                print ('output-max-diff gqa', i, context_seqlen, (out_ref - out_fa3_gqa).abs().max().item())
+                print ('output-mean-diff gqa',i, context_seqlen, (out_ref - out_fa3_gqa).abs().mean().item())
+                print ('lse-max-diff',i, context_seqlen, (lse_ref - lse_fa3).abs().max().item())
+                print ('lse-mean-diff',i,  context_seqlen, (lse_ref - lse_fa3).abs().mean().item())
+                print ('lse-max-diff gqa',i, context_seqlen, (lse_ref - lse_fa3_gqa).abs().max().item())
+                print ('lse-mean-diff gqa',i, context_seqlen, (lse_ref - lse_fa3_gqa).abs().mean().item())
 
-                assert ((out_fa2 - out_fa3).abs().max().item() <= 1e-3)
-                assert ((out_fa2 - out_fa3).abs().mean().item() <= 1e-4)
-                assert ((out_fa2 - out_fa3_gqa).abs().max().item() <= 1e-3)
-                assert ((out_fa2 - out_fa3_gqa).abs().mean().item() <= 1e-4)
-                assert ((lse_fa2 - lse_fa3).abs().max().item() <= 1e-3)
-                assert ((lse_fa2 - lse_fa3).abs().mean().item() <= 1e-4)
-                assert ((lse_fa2 - lse_fa3_gqa).abs().max().item() <= 1e-3)
-                assert ((lse_fa2 - lse_fa3_gqa).abs().mean().item() <= 1e-4)
+                assert ((out_ref - out_fa3).abs().max().item() <= 1e-3)
+                assert ((out_ref - out_fa3).abs().mean().item() <= 1e-4)
+                assert ((out_ref - out_fa3_gqa).abs().max().item() <= 1e-3)
+                assert ((out_ref - out_fa3_gqa).abs().mean().item() <= 1e-4)
+                assert ((lse_ref - lse_fa3).abs().max().item() <= 1e-3)
+                assert ((lse_ref - lse_fa3).abs().mean().item() <= 1e-4)
+                assert ((lse_ref - lse_fa3_gqa).abs().max().item() <= 1e-3)
+                assert ((lse_ref - lse_fa3_gqa).abs().mean().item() <= 1e-4)
 
 if __name__ == "__main__":
     main()
