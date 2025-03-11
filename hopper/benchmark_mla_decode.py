48b3acbc44e8fd66b804d695f526c2be3586a760
Tri Dao <tridpq@gmail.com>
2025-03-11 17:09:07 -0400

    Benchmark MHA, GQA, MQA, MLA in the same script

--- hopper/benchmark_mla_decode.py
+++ hopper/benchmark_mla_decode.py
@@ -1,3 +1,11 @@
+# Copyright (c) 2025, Ted Zadouri, Tri Dao.
+
+# We recommend locking GPU clocks before running the benchmark to ensure consistent results.
+# This can be done using the following commands (1830 MHz is the clock for H100):
+# sudo nvidia-smi -i 0 -pm 1
+# sudo nvidia-smi -i 0 --lock-gpu-clocks 1830,1830
+# See more here: https://github.com/triton-lang/triton/blob/d9f10ebdc5da53f73eb852fde73d8d7d80b679d1/python/triton/testing.py#L487
+
 import time
 import torch
 import torch.nn.functional as F
@@ -13,18 +21,19 @@ try:
 except ImportError:
     pytorch_profiler = None
 
+attn_variants = ["mha", "gqa", "mqa", "mla"]
+attn_variant = attn_variants[3]
 device = "cuda"
 dtype = torch.bfloat16
-# seqlen = 64 * 1024
 seqlen = 8192
 nheads = 128
-nheads_kv = 1
-headdim = 64
-headdim_v = 512
-has_qv = True
+nheads_kv = nheads if attn_variant == "mha" else (min(nheads // 8, 8) if attn_variant == "gqa" else 1)
+headdim = 64 if attn_variant == "mla" else 128
+headdim_v = 512 if attn_variant == "mla" else headdim
+has_qv = headdim == 64 and headdim_v == 512
 seqlen_q = 1
 # page_size = None
-page_size = 64
+page_size = 64 if attn_variant == "mla" else 128
 
 use_bench_cudagraph = False
 
@@ -35,23 +44,27 @@ cache_seqlens = None
 # cache_seqlens = torch.tensor([seqlen] * batch_size, device=device, dtype=torch.int)
 # cache_seqlens = torch.tensor([seqlen - 1, 1024, 1024, 1024], device=device, dtype=torch.int32)
 # cache_seqlens = torch.tensor([1024] * batch_size, device=device, dtype=torch.int)
-# cache_seqlens = torch.tensor([seqlen - 1, 1024, 1024, 1024], device=device, dtype=torch.int)
 # cache_seqlens = torch.tensor([4500, 45000, 1800, 1800], dtype=torch.int32, device=device)
+
+print(f"{attn_variant.upper()}, nheads_q = {nheads}, nheads_kv = {nheads_kv}, headdim = {headdim}, headdim_v = {headdim_v}, page_size = {page_size}")
 
 for seqlen in [s * 1024 for s in [1, 2, 4, 8, 16, 32, 64]]:
 # for seqlen in [s * 1024 for s in [1]]:
     cache_seqlens = torch.tensor([seqlen] * batch_size, device=device, dtype=torch.int)
     num_splits = 0
     q = torch.randn(batch_size, seqlen_q, nheads, headdim, dtype=dtype, device=device)
-    v_cache = torch.randn(batch_size, seqlen, nheads_kv, headdim_v, dtype=dtype, device=device)
-    k_cache = torch.randn(batch_size, seqlen, nheads_kv, headdim, dtype=dtype, device=device)
-    if page_size is not None:
-        assert seqlen % page_size == 0
-        k_cache, v_cache = [rearrange(x, "b (n p) h d -> (b n) p h d", p=page_size) for x in [k_cache, v_cache]]
-        page_table = rearrange(torch.arange(batch_size * seqlen // page_size, device=device, dtype=torch.int32),
-                            "(b s) -> b s", s=seqlen // page_size)
-    else:
-        page_table = None
+    try:
+        v_cache = torch.randn(batch_size, seqlen, nheads_kv, headdim_v, dtype=dtype, device=device)
+        k_cache = torch.randn(batch_size, seqlen, nheads_kv, headdim, dtype=dtype, device=device)
+        if page_size is not None:
+            assert seqlen % page_size == 0
+            k_cache, v_cache = [rearrange(x, "b (n p) h d -> (b n) p h d", p=page_size) for x in [k_cache, v_cache]]
+            page_table = rearrange(torch.arange(batch_size * seqlen // page_size, device=device, dtype=torch.int32),
+                                "(b s) -> b s", s=seqlen // page_size)
+        else:
+            page_table = None
+    except torch.OutOfMemoryError:
+        continue
     qv = torch.randn(batch_size, seqlen_q, nheads, headdim_v, dtype=dtype, device=device) if has_qv else None
 
     # Time in ms
@@ -65,12 +78,12 @@ for seqlen in [s * 1024 for s in [1, 2, 4, 8, 16, 32, 64]]:
     # exit(0)
 
     total_seqlen = seqlen * batch_size if cache_seqlens is None else cache_seqlens.sum().item()
-    mem_io = total_seqlen * nheads_kv * (headdim + headdim_v) * 2 + q.numel() * 2 + qv.numel() * 4
+    mem_io = total_seqlen * nheads_kv * (headdim + headdim_v) * 2 + q.numel() * 2 + (qv.numel() * 2 if has_qv else 0) + q.numel() * headdim_v // headdim * 2  # last time is for the output
     flops = seqlen_q * total_seqlen * nheads * (headdim + headdim_v * (2 if has_qv else 1)) * 2
     ideal_h100_time_mem = mem_io / 3.35e12 * 1e6
     ideal_h100_time_flop = flops / 989e12 * 1e6
     ideal_h100_time = max(ideal_h100_time_mem, ideal_h100_time_flop)
-    print(f"Time{'' if not use_bench_cudagraph else ' w CUDA Graph'}: {t0 * 1e3:.0f} us, {mem_io * 1e-9 / (t0 * 1e-3):.0f} GB/s, {flops * 1e-12 / (t0 * 1e-3):.0f} TFLOPS/s")
+    print(f"Seqlen = {seqlen}, time{'' if not use_bench_cudagraph else ' w CUDA Graph'}: {t0 * 1e3:.0f} us, {mem_io * 1e-9 / (t0 * 1e-3):.0f} GB/s, {flops * 1e-12 / (t0 * 1e-3):.0f} TFLOPS/s")
     print(f"Ideal time: {ideal_h100_time:.0f} us")
 
     # if pytorch_profiler is not None:
