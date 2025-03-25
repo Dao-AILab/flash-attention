8cbd1b42140ced0510ce3e6467674b7de39391bf
Lucas Wilkinson <LucasWilkinson@users.noreply.github.com>
2025-03-25 12:39:48 -0400

    Fix missing import in __init__ (#57)
    Signed-off-by: Lucas Wilkinson <lwilkinson@neuralmagic.com>

--- vllm_flash_attn/__init__.py
+++ vllm_flash_attn/__init__.py
@@ -4,6 +4,7 @@ __version__ = "2.7.2.post1"
 from .flash_attn_interface import (
     flash_attn_varlen_func,
     flash_attn_with_kvcache,
+    get_scheduler_metadata,
     sparse_attn_func,
     sparse_attn_varlen_func,
     is_fa_version_supported,
