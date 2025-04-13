24574e20e62404942a10e237a370e5c952664b84
liyong.0517 <liyong.0517@bytedance.com>
2025-04-13 15:52:36 +0800

    max 8
    Signed-off-by: liyong.0517 <liyong.0517@bytedance.com>

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ pip3 install requests psutil ninja
 pip3 install --no-cache-dir http://luban-source.byted.org/repository/scm/lab.pytorch.pytorch2_1.0.0.350.tar.gz
 
 cd hopper
-export MAX_JOBS=16 
+export MAX_JOBS=8 
 export FLASH_ATTENTION_FORCE_BUILD=TRUE
 export LDFLAGS=-L/usr/local/cuda/lib64/stubs
 python3 setup.py bdist_wheel
