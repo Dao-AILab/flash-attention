f5e38f7b12e798e95d0a25c4d9ba10e8617c2670
Tri Dao <tridao@users.noreply.github.com>
2025-05-21 21:42:14 -0700

    Update hopper/flash_api.cpp
    Co-authored-by: Jane (Yuan) Xu <31798555+janeyx99@users.noreply.github.com>

--- hopper/flash_api.cpp
+++ hopper/flash_api.cpp
@@ -4,7 +4,6 @@
 
 #include <Python.h>
 #include <torch/nn/functional.h>
-#include <torch/version.h>  // For TORCH_VERSION* macros
 #include <ATen/cuda/CUDAContext.h>
 #include <c10/cuda/CUDAGuard.h>
 
