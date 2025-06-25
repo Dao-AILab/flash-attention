f9633f7ff6d358f186216e554b867fe7780d479b
rocking <ChunYu.Lai@amd.com>
2025-06-25 05:16:01 +0000

    Revert "Merge pull request #147 from ROCm/poyenc/fix-ck-tile-splitkv-heuristic"
    This reverts commit 12857ce6d6874e7494dadb1289d36982f0017d6a, reversing
    changes made to e64b970304dd2f25e6264619f5af23824dcc43cc.

--- csrc/flash_attn_ck/flash_common.hpp
+++ csrc/flash_attn_ck/flash_common.hpp
@@ -35,15 +35,40 @@ inline __global__ void ParsePhiloxCudaState(at::PhiloxCudaState arg, uint64_t* r
     }
 }
 
-inline int num_splits_heuristic_ck(int batch_nheads_mblocks, int num_SMs, [[maybe_unused]] int num_n_blocks, int max_splits) {
+inline int num_splits_heuristic_ck(int batch_nheads_mblocks, int num_SMs, int num_n_blocks, int max_splits) {
     // If we have enough to almost fill the SMs, then just use 1 split
-    for (int num_splits = 1; num_splits <= max_splits; num_splits *= 2) {
-        if (num_SMs < batch_nheads_mblocks * (num_splits * 2)) {
+    if (batch_nheads_mblocks >= 0.8f * num_SMs) { return 1; }
+    max_splits = std::min({max_splits, num_SMs, num_n_blocks});
+    float max_efficiency = 0.f;
+    std::vector<float> efficiency;
+    efficiency.reserve(max_splits);
+    auto ceildiv = [](int a, int b) { return (a + b - 1) / b; };
+    // Some splits are not eligible. For example, if we have 64 blocks and choose 11 splits,
+    // we'll have 6 * 10 + 4 blocks. If we choose 12 splits, we'll have 6 * 11 + (-2) blocks
+    // (i.e. it's 11 splits anyway).
+    // So we check if the number of blocks per split is the same as the previous num_splits.
+    auto is_split_eligible = [&ceildiv, &num_n_blocks](int num_splits) {
+        return num_splits == 1 || ceildiv(num_n_blocks, num_splits) != ceildiv(num_n_blocks, num_splits - 1);
+    };
+    for (int num_splits = 1; num_splits <= max_splits; num_splits++) {
+        if (!is_split_eligible(num_splits)) {
+            efficiency.push_back(0.f);
+        } else {
+            float n_waves = float(batch_nheads_mblocks * num_splits) / num_SMs;
+            float eff = n_waves / ceil(n_waves);
+            // printf("num_splits = %d, eff = %f\n", num_splits, eff);
+            if (eff > max_efficiency) { max_efficiency = eff; }
+            efficiency.push_back(eff);
+        }
+    }
+    for (int num_splits = 1; num_splits <= max_splits; num_splits++) {
+        if (!is_split_eligible(num_splits)) { continue; }
+        if (efficiency[num_splits - 1] >= 0.85 * max_efficiency) {
+            // printf("num_splits chosen = %d\n", num_splits);
             return num_splits;
         }
     }
-
-    return max_splits;
+    return 1;
 }
 
 int override_num_splits_if_necessary(int batch, int nhead, int max_seqlen_q, int hdim_v, float p_drop, int num_splits);
