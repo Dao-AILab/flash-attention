2c8c2ff883ac146f56fc4fed1c08dc889f0d00ae
timmy-feng <timothy@modal.com>
2025-11-02 10:18:38 +0000

    Remove modal bench harness

--- flash_attn/__init__.py
+++ flash_attn/__init__.py
@@ -1,12 +1,11 @@
 __version__ = "2.8.3"
 
-# NOTE(timmy): Comment out these so that we can run cute version without unneeded dependencies
-# from flash_attn.flash_attn_interface import (
-#     flash_attn_func,
-#     flash_attn_kvpacked_func,
-#     flash_attn_qkvpacked_func,
-#     flash_attn_varlen_func,
-#     flash_attn_varlen_kvpacked_func,
-#     flash_attn_varlen_qkvpacked_func,
-#     flash_attn_with_kvcache,
-# )
+from flash_attn.flash_attn_interface import (
+    flash_attn_func,
+    flash_attn_kvpacked_func,
+    flash_attn_qkvpacked_func,
+    flash_attn_varlen_func,
+    flash_attn_varlen_kvpacked_func,
+    flash_attn_varlen_qkvpacked_func,
+    flash_attn_with_kvcache,
+)
