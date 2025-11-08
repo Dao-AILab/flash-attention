9658628cd10d8e6ec3cb246f81883f110b7100c6
Johnny <johnnynuca14@gmail.com>
2025-11-08 23:44:31 +0100

    revert changes

--- flash_attn/cute/interface.py
+++ flash_attn/cute/interface.py
@@ -289,10 +289,7 @@ def _flash_attn_fwd(
         if _compute_capability is None
         else _compute_capability
     )
-    # Map SM120 (compute capability 12) to SM100 kernels since they're both Blackwell variants
-    if compute_capability == 12:
-        compute_capability = 10
-    assert compute_capability in [9, 10], "Unsupported compute capability. Supported: 9.x, 10.x, 12.x (uses SM100 kernels)"
+    assert compute_capability in [9, 10], "Unsupported compute capability. Supported: 9.x, 10.x"
     current_stream = cuda.CUstream(torch.cuda.current_stream().cuda_stream)
 
     if compute_capability == 9:  # TODO: tune block size according to hdim.
@@ -551,10 +548,7 @@ def _flash_attn_bwd(
     seqused_k: Optional[torch.Tensor] = None,
 ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
     compute_capability = torch.cuda.get_device_capability()[0]
-    # Map SM120 (compute capability 12) to SM100 kernels since they're both Blackwell variants
-    if compute_capability == 12:
-        compute_capability = 10
-    assert compute_capability in [9, 10], "Unsupported compute capability. Supported: 9.x, 10.x, 12.x (uses SM100 kernels)"
+    assert compute_capability in [9,10], "Unsupported compute capability. Supported: 9.x, 10.x"
 
     if compute_capability == 9:
         m_block_size = 80 if not causal else 64
