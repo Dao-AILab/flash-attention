58e0626a692f09241182582659e3bf8f16472659
Matthew Bonanni <mbonanni@redhat.com>
2025-11-13 13:48:35 -0600

    Remove bindings for flash_attn_with_kvcache (#107)
    * remove method
    
    Signed-off-by: Matthew Bonanni <mbonanni@redhat.com>
    
    * disable AppendKV
    
    Signed-off-by: Matthew Bonanni <mbonanni@redhat.com>
    
    * remove from init
    
    Signed-off-by: Matthew Bonanni <mbonanni@redhat.com>
    
    ---------
    
    Signed-off-by: Matthew Bonanni <mbonanni@redhat.com>

--- vllm_flash_attn/__init__.py
+++ vllm_flash_attn/__init__.py
@@ -3,7 +3,6 @@ __version__ = "2.7.2.post1"
 # Use relative import to support build-from-source installation in vLLM
 from .flash_attn_interface import (
     flash_attn_varlen_func,
-    flash_attn_with_kvcache,
     get_scheduler_metadata,
     sparse_attn_func,
     sparse_attn_varlen_func,
