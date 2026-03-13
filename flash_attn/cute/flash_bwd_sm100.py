a98773814fb9ddc291b8f665eb9949f655c7f167
Inoday Yadav <yadavinoday@gmail.com>
2026-03-13 17:04:35 -0400

    Support 2CTA for sliding window hdim 192

--- flash_attn/cute/flash_bwd_sm100.py
+++ flash_attn/cute/flash_bwd_sm100.py
@@ -84,7 +84,7 @@ class FlashAttentionBackwardSm100:
         self.use_2cta_instrs = bool(
             use_2cta_instrs
             and cluster_size == 2
-            and not is_local
+            and (not is_local or self.tile_hdim == 192)
             and score_mod is None
             and score_mod_bwd is None
             and mask_mod is None
@@ -929,8 +929,11 @@ class FlashAttentionBackwardSm100:
                 "2-CTA mode does not support block sparsity. "
                 "Please create kernel with use_2cta_instrs=False for block sparse attention."
             )
-            assert window_size_left is None and window_size_right is None, (
-                "2-CTA mode does not support window attention. "
+            assert (
+                self.tile_hdim == 192
+                or (window_size_left is None and window_size_right is None)
+            ), (
+                "2-CTA mode does not support window attention for hdim != 192."
                 "Please create kernel with use_2cta_instrs=False for window attention."
             )
         # 2-CTA: 231424 and 1-CTA: 232448
