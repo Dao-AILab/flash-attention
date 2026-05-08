0327fa16c41f85ab753a5e74179692f601ebd2d9
Björn Buschkämper <bjoern.buschkaemper@gmail.com>
2026-05-08 09:57:38 +0000

    Export hidden interface functions as well in fa3 interface.
    Signed-off-by: Björn Buschkämper <bjoern.buschkaemper@gmail.com>

--- hopper/flash_attn_interface.py
+++ hopper/flash_attn_interface.py
@@ -1,1 +1,5 @@
+from flash_attn_3 import flash_attn_interface as _flash_attn_interface
 from flash_attn_3.flash_attn_interface import *  # noqa: F403
+
+_flash_attn_forward = _flash_attn_interface._flash_attn_forward
+_flash_attn_backward = _flash_attn_interface._flash_attn_backward
