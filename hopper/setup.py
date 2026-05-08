1664622480512ee881775277f734bff415df9529
Shivang <40532945+aw920h@users.noreply.github.com>
2026-05-09 00:45:40 +0530

    Brought back Py_LIMITED_API flag to CUDA extension compilation

--- hopper/setup.py
+++ hopper/setup.py
@@ -646,7 +646,7 @@ if not SKIP_CUDA_BUILD:
             name=f"{PACKAGE_NAME}._C",
             sources=sources,
             extra_compile_args={
-                "cxx":["-O3", "-std=c++17"] + stable_args + feature_args,
+                "cxx":["-O3", "-std=c++17", "-DPy_LIMITED_API=0x03090000"] + stable_args + feature_args,
                 "nvcc": nvcc_threads_args() + nvcc_flags + cc_flag + feature_args,
             },
             include_dirs=include_dirs,
